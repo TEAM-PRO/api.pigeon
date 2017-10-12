b826fbed96f9ebe5d698bc8bb5124b3cbc5beb86
unknown <borodatuu@gmail.com>
2017-10-13 01:23:00 +0300

    base_structure

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const app = express();
+const secret = require('./../config/jwt.secretkey.json');
+const { messages } = require('./helper');
+
+const token = req => req.headers['x-access-token'];
+const tokenFreeURLs = ['/api/user/activation', '/api/users', '/api/support', '/api/auth/login'];
+const checkURL = baseUrl => tokenFreeURLs.some(URL => baseUrl.match(URL));
+const verifyToken = (token, req, next) => jwt.verify(token, secret.key, (err, decoded) =>
+    err && res.status(401).json({ 'message': messages.failedToken }) ||
+    (req.decoded = decoded) && next()
+);
+
+app.use(logger('dev'));
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(bodyParser.urlencoded({ limit: '2mb', extended: true }));
+app.use(cors());
+
+app.use('*', (req, res, next) => {
+    !token(req) && !checkURL(req.baseUrl) && res.status(401).json({ 'message': messages.noToken }) ||
+        token(req) && verifyToken(token(req), req, next) || checkURL(req.baseUrl) && next();
+});
+
+require('./routes')(app);
+
+module.exports = app;
---
+++ server/helper/passwordGenerator.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+    passwordGenerate() {
+        const chars = ['qwertyuiopasdfghjklzxcvbnm', 'QWERTYUIOPASDFGHJKLZXCVBNM', '#?!@$%^&*-', '1234567890'];
+        let password = '';
+
+        for (let i = 0; i < 3; i++) {
+            password += chars.map(char => char.charAt(Math.floor(Math.random() * char.length))).join('');
+        }
+        return password;
+    }
+};
