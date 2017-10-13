ec357500b05a9c2c189ce4e10070a33b15606e03
unknown <borodatuu@gmail.com>
2017-10-13 22:46:43 +0300

    make change in package.json and routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,5 @@ const {
 } = require('../controllers');
 
 module.exports = app => {
-    app.get('/api/profile/:id', profileController.retrieve);
-    app.put('/api/profile/:id', profileController.update);
 
-    app.post('/api/users', userController.create);
-    app.post('/api/user/changepassword', userController.update);
-    app.get('/api/user/:user_id', userController.retrieve);
-    app.delete('/api/user/:user_id', userController.destroy);
 };
