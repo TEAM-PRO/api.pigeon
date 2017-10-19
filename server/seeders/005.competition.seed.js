edffc5f432c67ae8df8b4c4a946d90a0833f2c01
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-10-19 17:11:30 +0300

    Added seed file for the Competition model

---
+++ server/seeders/005.competition.seed.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.bulkInsert('Competitions', [
+      {
+        name: "First Ukraine's big competition",
+        date: Date.parse('2017-12-21 15:00:00.00+03'),
+        plase: "Lviv city, Valova street",
+        firstPlaceWinnerId: 1,
+        secondPlaceWinnerId: 2,
+        thirdPlaceWinnerId: 3,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+      }
+    ]),
+  down: (queryInterface, Sequelize) =>
+    queryInterface.bulkDelete('Competitions', null)
+};
