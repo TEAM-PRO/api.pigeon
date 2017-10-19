7261f67ddf0bb22eb754291fe92bac0cf936b99a
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-10-19 15:03:48 +0300

    Corrected mistake

--- server/migrations/20171016203203-create-pigeon.js
+++ server/migrations/20171016203203-create-pigeon.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Pigeon', {
+    return queryInterface.createTable('Pigeons', {
         id: {
           allowNull: false,
           autoIncrement: true,
@@ -52,7 +52,6 @@ module.exports = {
       });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Pigeon');
+    return queryInterface.dropTable('Pigeons');
   }
 };
-
