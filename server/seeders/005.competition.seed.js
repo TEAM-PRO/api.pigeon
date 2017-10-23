d3f884ae63889eca4970e67eec8589c94bb085d9
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-10-23 18:18:48 +0300

    Corrected plase wrong word

--- server/seeders/005.competition.seed.js
+++ server/seeders/005.competition.seed.js
@@ -6,7 +6,7 @@ module.exports = {
       {
         name: 'First world big competition',
         date: Date.parse('2017-12-21 15:00:00.00+03'),
-        plase: 'Lviv city, Valova street',
+        place: 'Lviv city, Valova street',
         firstPlaceWinnerId: 1,
         secondPlaceWinnerId: 2,
         thirdPlaceWinnerId: 3,
