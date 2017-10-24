d2a4ec01017b5536bd4502b82e2106657cd025cd
Yuriy <borodatuu@gmail.com>
2017-10-24 14:34:22 +0300

    hot fix

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,7 @@
 const profileController = require('./profile.controller');
 const userController = require('./user.controller');
-const pigeonController = require('./pigeon.controller');
 
 module.exports = {
   profileController,
   userController,
-  pigeonController
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,8 @@
 const {
     profileController,
-    userController,
-    pigeonController
+    userController
 } = require('../controllers');
 
 module.exports = app => {
-  app.post('/api/pigeon', pigeonController.create);
-  app.post('/api/pigeon/:pigeon_id', pigeonController.update);
-  app.get('/api/pigeon/:pigeon_id', pigeonController.retrieve);
-  app.delete('/api/pigeon/:pigeon_id', pigeonController.destroy);
+
 };
