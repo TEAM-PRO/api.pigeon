652576f484d74422db4b14533459c52bab6c436b
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-10-24 13:48:59 +0300

    Corrected bad id in comment seed file

--- server/seeders/006.comment.seed.js
+++ server/seeders/006.comment.seed.js
@@ -15,7 +15,7 @@ module.exports = {
         userId: 1,
         pigeonId: 2,
         date:  Date.parse('2017-10-01 16:00:00.00+03'),
-        body: 'When will be next exhibition?',
+        body: 'When will be the next exhibition?',
         createdAt: Date.now(),
         updatedAt: Date.now()
       },
@@ -23,15 +23,15 @@ module.exports = {
         userId: 2,
         pigeonId: 1,
         date:  Date.parse('2017-10-01 17:30:00.00+03'),
-        body: 'I want buy pigeon "Charly"',
+        body: 'I want to buy pigeon "Charly"',
         createdAt: Date.now(),
         updatedAt: Date.now()
       },
       {
-        userId: 3,
+        userId: 2,
         pigeonId: 1,
         date:  Date.parse('2017-10-02 15:00:00.00+03'),
-        body: 'How mach cost pigeon "Charly"?',
+        body: 'How much money does pigeon "Charly" cost?',
         createdAt: Date.now(),
         updatedAt: Date.now()
       }
